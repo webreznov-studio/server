75c19a08dacc7627d2e0337d29be98d29484b598
webreznov-studio <73492632+webreznov-studio@users.noreply.github.com>
2020-10-29 22:12:04 +0300

    refactor

--- index.js
+++ index.js
@@ -7,16 +7,6 @@ let nodemailer = require('nodemailer');
 // Static folder
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
-// const transporter = nodemailer.createTransport({
-//   host: "smtp.gmail.com", 
-//     port: 465,
-//     secure: false,
-//   auth: {
-//     user: "remizov.cs@gmail.com",
-//     pass: "jkl32460101" 
-//   }
-// });
-
 let transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 465,
@@ -26,14 +16,6 @@ let transporter = nodemailer.createTransport({
     pass: "rez391)M", // generated ethereal password
   },
 });
-
-// let transporter = nodemailer.createTransport({
-//   sendMail: true,
-//   auth: {
-//     user: "webreznov.landing@gmail.com", // generated ethereal user
-//     pass: "rez391)M", // generated ethereal password
-//   },
-// });
 
 // verifying the connection configuration
 transporter.verify(function(error, success) {
@@ -64,7 +46,7 @@ app.post('/access', (req, res, next) => {
   transporter.sendMail(mail, (err, data) => {
     if (err) {
       res.json({
-        status: 'fail'
+        status: 'fail. Sorry, mail not send'
       })
     } else {
       res.json({
