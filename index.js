f60d662d326bf1c9da095449a7b1a99b7f575012
webreznov-studio <73492632+webreznov-studio@users.noreply.github.com>
2020-11-10 10:07:58 +0300

    CORS politic

--- index.js
+++ index.js
@@ -10,6 +10,12 @@ app.use('/public', express.static(path.join(__dirname, 'public')));
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 let transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 465,
